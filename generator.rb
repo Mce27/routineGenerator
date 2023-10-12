0a41f68c364f9ee668c4e918784aeec75139f557
mce27 <matthewemerick1@gmail.com>
2023-10-12 10:53:18 -0400

    Routine.readFigures now works

--- generator.rb
+++ generator.rb
@@ -14,7 +14,7 @@ class Figure
     end
 
     def data
-      data = {'name':@name,
+      return data = {'name':@name,
       'startDirection':@startDirection,
       'endDirection':@endDirection,
       'style':@style,
@@ -24,6 +24,14 @@ class Figure
       'startFoot':@startFoot,
       'endFoot':@endFoot,
       'level':@level}
+    end
+    def to_s
+      out = ''
+      d = self.data
+      for i in d.keys
+        out += i.to_s + ":" + d[i] + ', '
+      end
+      return out
     end
 end
 
@@ -53,19 +61,22 @@ class Routine
     figures = file.readlines
     #Build @figureDB
     for line in figures
-      name, startDirection, endDirection, style, dance, endPosition, timing, startFoot, endFoot, level = line.split','.strip
-      figure = Figure.new(name, startDirection, endDirection, style, dance, endPosition, timing, startFoot, endFoot, level)
+      if line.start_with?"#"
+        next
+      end
+      name, startDirection, endDirection, style, dance, endPosition, timing, startFoot, endFoot, level = line.split','
+      figure = Figure.new(name.to_s.strip, startDirection.to_s.strip, endDirection.to_s.strip, style.to_s.strip, dance.to_s.strip, endPosition.to_s.strip, timing.to_s.strip, startFoot.to_s.strip, endFoot.to_s.strip, level.to_s.strip)
       fig_data = figure.data
       if !@figureDB.has_key?(fig_data[:level])
-        @figureDB[level] = {}
+        @figureDB[fig_data[:level]] = {}
       end
-      if !@figureDB[level].has_key?(fig_data[:style])
-        @figureDB[level][style] = {}
+      if !@figureDB[fig_data[:level]].has_key?(fig_data[:style])
+        @figureDB[fig_data[:level]][fig_data[:style]] = {}
       end
-      if !@figure[level][style].has_key?(fig_data[:dance])
-        @figure[level][style][dance] = []
+      if !@figureDB[fig_data[:level]][fig_data[:style]].has_key?(fig_data[:dance])
+        @figureDB[fig_data[:level]][fig_data[:style]][fig_data[:dance]] = []
       end
-      @figure[level][style][dance].push figure
+      @figureDB[fig_data[:level]][fig_data[:style]][fig_data[:dance]].push figure
     end
   end
   def figureDB
@@ -88,4 +99,8 @@ end
 
 r = Routine.new("STD","WATLZ",[10,10],"N",1,"I","Long","DW")
 r.readFigures
-puts r.figureDB
+#puts r.figureDB['N']["STD"]
+d = r.figureDB['N']["STD"]["WALTZ"]
+for el in d
+  puts el.to_s
+end
